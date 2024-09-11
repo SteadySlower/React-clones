92f42a12af50e0229ad7ceaff168d1308f3f3ca4
JW Moon <moon910904@gmail.com>
2024-09-11 16:39:53 +0900

    feature: add router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppYoutube from "./youtube-clone/AppYoutube";
+import Home from "./Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/youtube",
+        element: <AppYoutube />,
+    },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <RouterProvider router={router}>
+            <Home />
+        </RouterProvider>
+    );
 }
 
 export default App;
---
+++ src/youtube-clone/AppYoutube.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppYoutube(props) {
+    return <div>AppYoutube</div>;
+}
+
+export default AppYoutube;
