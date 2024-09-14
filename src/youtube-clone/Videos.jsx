eb16183360153e254076266be63727407a19b5d3
JW Moon <moon910904@gmail.com>
2024-09-14 14:36:28 +0900

    feature: add fetching logic

--- src/youtube-clone/Videos.jsx
+++ src/youtube-clone/Videos.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Videos(props) {
     const { keyword } = useParams();
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        // const url = `youtube-mock/${keyword ? "keyword" : "hot-trend"}.json`;
+        const url = `youtube-mock/keyword.json`;
+        console.log("url: ", url);
+        setLoading(true);
+        setError(undefined);
+        fetch(url)
+            .then((res) => {
+                console.log("res: ", res);
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setVideos(data.items);
+            })
+            .catch((e) => {
+                console.log(e);
+                setError(e);
+            })
+            .finally(() => setLoading(false));
+    }, [keyword]);
+
     return (
-        <div>
-            {keyword ? `Videos of query ${keyword}` : "Videos 🔥"}
-        </div>
+        <ul>
+            <li>키워드: {keyword}</li>
+            {videos.map((video) => (
+                <li key={video.etag}>{video.snippet.title}</li>
+            ))}
+        </ul>
     );
 }
 
