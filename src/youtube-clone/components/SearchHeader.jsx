789453a87d1e7638a25d5f41c3f13cd82db952bd
JW Moon <moon910904@gmail.com>
2024-09-14 13:52:07 +0900

    design: add css in SearchHeader

--- src/youtube-clone/components/SearchHeader.jsx
+++ src/youtube-clone/components/SearchHeader.jsx
@@ -22,22 +22,24 @@ function SearchHeader() {
     }, [keyword]);
 
     return (
-        <header>
-            <Link to={"/youtube"}>
-                <div>
-                    <BsYoutube />
-                    <h1>Youtube</h1>
-                </div>
+        <header className="w-full flex p-4 text-2xl border-b border-zinc-600 mb-4">
+            <Link to={"/youtube"} className="flex items-center">
+                <BsYoutube className="text-4xl text-youtubeBrand" />
+                <h1 className="font-bold ml-2 text-3xl">Youtube</h1>
             </Link>
 
-            <form onSubmit={handleSubmit}>
+            <form
+                onSubmit={handleSubmit}
+                className="w-full flex justify-center"
+            >
                 <input
+                    className="w-7/12 p-2 outline-none bg-black text-gray-50"
                     type="text"
                     placeholder="Search Video..."
                     value={query}
                     onChange={handleChange}
                 />
-                <button>
+                <button className="bg-zinc-600 px-4">
                     <BsSearch />
                 </button>
             </form>
