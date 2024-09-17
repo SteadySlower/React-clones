d287afcf8621dbafbfbd3ac8962414ffab1ce89c
JW Moon <moon910904@gmail.com>
2024-09-17 15:01:55 +0900

    feature: add design in VideoCard

--- src/youtube-clone/components/VideoCard.jsx
+++ src/youtube-clone/components/VideoCard.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { formatAgo } from "../util/date";
 
+// string에 #&39 같은 형태의 html 특수문자가 있는 경우 해결하는 방법
+    // html 문서 안에 넣었다가 빼준다.
+function decodeHTMLEntities(text) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = text;
+    return textArea.value;
+}
+
 function VideoCard({ video }) {
     const {
         title,
@@ -12,11 +20,15 @@ function VideoCard({ video }) {
     } = video.snippet;
     return (
         <div className="flex-col">
-            <img src={url} alt="thumbnail img" />
+            <img className="w-full" src={url} alt="thumbnail img" />
             <div>
-                <p>{title}</p>
-                <p>{channelTitle}</p>
-                <p>{formatAgo(publishedAt, "ko")}</p>
+                <p className="font-semibold my-2 line-clamp-2">
+                    {decodeHTMLEntities(title)}
+                </p>
+                <p className="text-sm opacity-80">{channelTitle}</p>
+                <p className="text-sm opacity-80">
+                    {formatAgo(publishedAt, "ko")}
+                </p>
             </div>
         </div>
     );
