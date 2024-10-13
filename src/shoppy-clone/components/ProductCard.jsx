2fdd1100d1c789106001856bcf805831dea273a9
JW Moon <moon910904@gmail.com>
2024-10-13 14:07:22 +0900

    feature: add Product Detail page

--- src/shoppy-clone/components/ProductCard.jsx
+++ src/shoppy-clone/components/ProductCard.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function ProductCard({ product: { id, image, title, category, price } }) {
+function ProductCard({
+    product, // 이렇게 하면 풀어서 가져온 property 외에도 쓸 수 있다.
+    product: { id, image, title, category, price },
+}) {
+    const navigate = useNavigate();
     return (
-        <li className="rounded-lg shadow-md overflow-hidden cursor-pointer">
+        <li
+            onClick={() => {
+                navigate(`/shoppy/products/${id}`, { state: { product } });
+            }}
+            className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
+        >
             <img className="w-full" src={image} alt={title} />
             <div className="mt-2  px-2 text-lg flex justify-between items-center">
                 <h3 className="truncate">{title}</h3>
