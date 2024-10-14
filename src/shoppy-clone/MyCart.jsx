2cfdd02a846a1c46529166a0b75bb12883b722cf
JW Moon <moon910904@gmail.com>
2024-10-14 09:36:44 +0900

    feature: add MyCart ui

--- src/shoppy-clone/MyCart.jsx
+++ src/shoppy-clone/MyCart.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React from "react";
+import CartItem from "./components/CartItem";
+import Button from "./components/ui/Button";
+import { getCart } from "./api/firebase";
+import { useQuery } from "@tanstack/react-query";
+import { useAuthContext } from "./context/AuthContext";
+import { BsFillPlusCircleFill } from "react-icons/bs";
+import PriceCard from "./components/PriceCard";
+import { FaEquals } from "react-icons/fa";
+
+const SHIPPING_PRICE = 3000;
 
 function MyCart(props) {
+    const { uid } = useAuthContext();
+    const { data: products, isLoading } = useQuery({
+        queryKey: ["carts"],
+        queryFn: () => getCart(uid),
+    });
+    if (isLoading) return <p>Loading...</p>;
+    const hasProducts = products && products.length > 0;
+    const totalPrice =
+        products &&
+        products.reduce(
+            (prev, current) =>
+                prev + parseInt(current.price) * current.quantity,
+            0
+        );
     return (
-        <div>
-            My Cart
-        </div>
+        <section className="p-8 flex flex-col">
+            <p className="text-xl text-center font-bold pb-4 border-b border-gray-300">
+                내 장바구니
+            </p>
+            {!hasProducts && <p>장바구니에 제품이 없습니다.</p>}
+            {hasProducts && (
+                <>
+                    <ul className="border-b border-gray-300 mb-8 p-4 px-8">
+                        {products &&
+                            products.map((product) => (
+                                <CartItem
+                                    key={product.id}
+                                    product={product}
+                                    uid={uid}
+                                />
+                            ))}
+                    </ul>
+                    <div className="flex justify-between items-center mb-6 px-2 md:px-8 lg:px-16">
+                        <PriceCard text="상품 총액" price={totalPrice} />
+                        <BsFillPlusCircleFill className="shrink-0" />
+                        <PriceCard text="배송비" price={SHIPPING_PRICE} />
+                        <FaEquals className="shrink-0" />
+                        <PriceCard
+                            text="총가격"
+                            price={totalPrice + SHIPPING_PRICE}
+                        />
+                    </div>
+                    <Button text="주문하기" />
+                </>
+            )}
+        </section>
     );
 }
 
---
+++ src/shoppy-clone/components/PriceCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function PriceCard({ text, price }) {
+    return (
+        <div className="bg-gray-50 p-8 rounded-2xl text-center text-lg md:text-xl">
+            <p>{text}</p>
+            <p className="font-bold text-shoppyBrand text-xl md:text-2xl">
+                ₩{price}
+            </p>
+        </div>
+    );
+}
+
+export default PriceCard;
